8f2ab507aeb3f888781d27dc896cead19b2f6efb
Shaka004 <hamzahashfa2@gmail.com>
2023-10-26 16:11:59 +0700

    posujian

---
+++ home.php
@@ -0,0 +1,13 @@
+<div>
+    <h2><center>Pos Ujian Akhir Kelas TKJ RGI Angkatan XXIX</center></h2>
+    <img src="gambar/tampilan.webp" alt="TKJ" width="90%" class="rounded mx-auto d-block">
+    <p>Ujian Akhir Kelas TKJ Tentang Materi Web Development.</p>
+    <ul>
+        <li>Buat CRUD untuk Aplikasi POS (Point Of Sales)</li>
+        <li>Buat Database dengan nama: posujian terdiri dari 2 tabel, yaitu tabel supplier dan tabel penjualan</li>
+        <li>Buat CRUD untuk supplier pastikan jalan semua</li>
+        <li>Buat CRUD untuk penjualan pastikan jalan semua</li>
+        <li>NB: Screenshot setiap nomor dan masukan kodenya github.</li>
+    </ul>
+    <p>Ujian Akhir WebDev Angkatan XXIX Adalah Membuat CRUD Aplikasi POS(Point Of Sales).</p>
+</div>
